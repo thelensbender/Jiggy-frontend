bf988ef32d525ed25b1911ec4dbd0d80cf2b34be
Ijinleifeoluwa <ijinleifeshadare@gmail.com>
2026-04-21 21:19:32 +0100

    feat: implement WelcomeScreen and refactor App styling
    - Added WelcomeScreen component
    - Updated App.jsx and App.css for new layout
    - Cleaned up unused assets (hero.png, react.svg, vite.svg)
    - Updated dependencies in package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,42 @@
+import { useState } from "react";
+import WelcomeScreen from "./components/WelcomeScreen";
+
 export default function App() {
-  return (
-    <div className="text-3xl text-blue-500">
-      StreakFlow is working 🚀
-    </div>
+   // defaultMetrics is a variable of dummy values
+   const defaultMetrics = [{
+      id: "metric-1",
+      name: "Exercise",
+      unit: "Days",
+      streak: 5,
+      entries: [
+         {date: "18-01-2005", completed: true},
+         {date: "19-01-2005", completed: true}
+      ]
+   }, {
+      id: "metric-2",
+      name: "Coding Practice",
+      unit: "Days",
+      streak: 13,
+      entries: [
+         {date: "18-01-2005", completed: true},
+         {date: "19-01-2005", completed: true}
+      ]
+   }, {
+      id: "metric-3",
+      name: "Prayer",
+      unit: "Days",
+      streak: 0,
+      entries: [
+         {date: "18-01-2005", completed: true},
+         {date: "19-01-2005", completed: false}
+      ]
+   }]
+
+
+   const [metrics, setMetrics] = useState(defaultMetrics);
+   return (
+      <>
+         <WelcomeScreen></WelcomeScreen>
+      </>
   );
 }
